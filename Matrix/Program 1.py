dd89063fdb160b4ab51200b0cde523d042312899
Murshid TP <64684651+Murshidtp@users.noreply.github.com>
2022-06-09 13:13:23 +0530

    DS Lab

--- Matrix/Program 1.py
+++ Matrix/Program 1.py
@@ -1,5 +1,4 @@
-#Find maximun and minimum value in a matrix
-
+# Find maximun and minimum value in a matrix
 import numpy as np
 matrix = np.array([[2,4,6],[7,3,1],[4,8,2]])
 print("Minimum value:",np.min(matrix))
